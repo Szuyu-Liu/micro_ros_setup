1cc4444a8fdd556ecd27a27a43a5e48303523120
Cerber927 <149463942+Cerber927@users.noreply.github.com>
2025-02-27 15:50:32 +0100

    add floatarray topic /car/command

--- main/main.ino
+++ main/main.ino
@@ -9,14 +9,18 @@
 #include <rclc/executor.h>
 
 #include <std_msgs/msg/float32.h>
+#include <std_msgs/msg/float32_multi_array.h>
 
 CarController car;
 String command;
 
 rcl_subscription_t speed_sub, distance_sub, direction_sub;
+rcl_subscription_t car_sub;
+
 std_msgs__msg__Float32 msgspeed;
 std_msgs__msg__Float32 msgdistance;
 std_msgs__msg__Float32 msgdirection;
+std_msgs__msg__Float32MultiArray msg;
 
 rclc_executor_t executor;
 rclc_support_t support;
@@ -41,6 +45,16 @@ void direction_subscription_callback(const void * msgin)
   command = command+"direction:"+String(msgdirection->data);
 }
 
+void car_subscription_callback(const void * msgin)
+{
+  const std_msgs__msg__Float32MultiArray * msg = (const std_msgs__msg__Float32MultiArray *)msgin;
+  for (size_t i = 0; i < msg->data.size; i++) {
+    if (msg->data.data[0] <= 99) command = command+"speed:"+String(msg->data.data[0]);
+    if (msg->data.data[1] <= 99) command = command+"distance:"+String(msg->data.data[1]);
+    if (msg->data.data[2] <= 99) command = command+"direction:"+String(msg->data.data[2]);
+  }
+}
+
 void setup() {
 
   car.initialize();
@@ -57,7 +71,11 @@ void setup() {
   // create node
   rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support);
 
-   // create subscriber
+  msg.data.capacity = 4; // Adjust size as needed
+  msg.data.size = 0;
+  msg.data.data = (float *)malloc(msg.data.capacity * sizeof(float));
+
+  // create subscriber
   rclc_subscription_init_default(
     &speed_sub,
     &node,
@@ -76,11 +94,18 @@ void setup() {
     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
     "car/direction");
 
+  rclc_subscription_init_default(
+    &car_sub,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray),
+    "car/command");
+
   // create executor
-  rclc_executor_init(&executor, &support.context, 3, &allocator);
+  rclc_executor_init(&executor, &support.context, 4, &allocator);
   rclc_executor_add_subscription(&executor, &speed_sub, &msgspeed, &speed_subscription_callback, ON_NEW_DATA);
   rclc_executor_add_subscription(&executor, &distance_sub, &msgdistance, &distance_subscription_callback, ON_NEW_DATA);
   rclc_executor_add_subscription(&executor, &direction_sub, &msgdirection, &direction_subscription_callback, ON_NEW_DATA);
+  rclc_executor_add_subscription(&executor, &car_sub, &msg, &car_subscription_callback, ON_NEW_DATA);
 }
 
 void loop() {
